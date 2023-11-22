b10542f8adff7b657f06a4586c1dba8924a61fd6
Mr.Meeseeks <emil485h@gmail.com>
2023-11-22 13:53:19 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PrisPumpen',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
