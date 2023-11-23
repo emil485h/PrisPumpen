aaecec8c138c5d614f1fc44be34ba7a39f752b52
emil485h <emil485h@gmail.com>
2023-11-23 11:51:26 +0100

    Kamera function added

---
+++ src/app/kamera/kamera.page.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { PhotoService, UserPhoto } from '../services/photo.service';
+import { ActionSheetController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-kamera',
+  templateUrl: './kamera.page.html',
+  styleUrls: ['./kamera.page.scss'],
+})
+export class KameraPage implements OnInit {
+
+  constructor(public photoService: PhotoService,
+              public actionSheetController: ActionSheetController) { }
+              
+              public async showActionSheet(photo: UserPhoto, position: number) {
+                const actionSheet = await this.actionSheetController.create({
+                  header: 'Photos',
+                  buttons: [{
+                    text: 'Delete',
+                    role: 'destructive',
+                    icon: 'trash',
+                    handler: () => {
+                      this.photoService.deletePicture(photo, position);
+                    }
+                  }, {
+                    text: 'Cancel',
+                    icon: 'close',
+                    role: 'cancel',
+                    handler: () => {
+                      // Nothing to do, action sheet is automatically closed
+                      }
+                  }]
+                });
+                await actionSheet.present();
+              }
+              
+addPhotoToGallery() {
+  this.photoService.addNewToGallery();
+}
+
+  async ngOnInit() {
+    await this.photoService.loadSaved();
+  }
+
+}
