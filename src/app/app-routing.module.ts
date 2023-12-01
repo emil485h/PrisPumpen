698dc7a7530fc1a17f67beec9f1d21df5452d7d8
Mr.Meeseeks <emil485h@gmail.com>
2023-12-01 14:21:52 +0100

    Forside Elements added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,26 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { KameraPage } from './kamera/kamera.page';
+import { ForsidePage } from './forside/forside.page';
 
 
 const routes: Routes = [
- {
-    path: 'kamera',
-    loadChildren: () => import('./kamera/kamera.module').then( m => m.KameraPageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'kamera',
-    pathMatch: 'full'
-  }, 
   
   {
     path: 'forside',
     loadChildren: () => import('./forside/forside.module').then( m => m.ForsidePageModule)
   },
+ 
+  {
+    path: '',
+    redirectTo: 'forside',
+    pathMatch: 'full'
+  }, 
+  {
+    path: 'kamera',
+    loadChildren: () => import('./kamera/kamera.module').then( m => m.KameraPageModule)
+  },
+  
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
