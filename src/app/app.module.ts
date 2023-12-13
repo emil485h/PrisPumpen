69342893018d810c81cbffe8a55869f67b04693b
Mr.Meeseeks <emil485h@gmail.com>
2023-12-13 11:19:37 +0100

    Merge fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { RouteReuseStrategy } from '@angular/router';
 import { AngularFireModule } from '@angular/fire/compat';
 import { FirestoreModule } from '@angular/fire/firestore';
 import { environment } from 'src/environments/environment';
-
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
@@ -14,7 +14,7 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AngularFireModule.initializeApp(environment.firebaseConfig),
-    FirestoreModule],
+    FirestoreModule, AngularFireAuthModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,15 +9,5 @@ export const environment = {
     messagingSenderId: "601731091832",
     appId: "1:601731091832:web:165b51ee7a1a090a6f4c8d",
     measurementId: "G-NP7CV2BDD4"
-  },
-  firebaseConfig2 : {
-    apiKey: "AIzaSyC9cECVhuPBWCNjUWfVM21AFxBIhVgd8cU",
-    authDomain: "prispumpen-f2e82.firebaseapp.com",
-    databaseURL: "https://prispumpen-f2e82-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "prispumpen-f2e82",
-    storageBucket: "prispumpen-f2e82.appspot.com",
-    messagingSenderId: "547779728309",
-    appId: "1:547779728309:web:5f24d4e2c4a3ca10203ffc",
-    measurementId: "G-NW3N20L0HV"
   }
 };
